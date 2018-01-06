342184484d3923e623d0d75e724d283b22df9b17
makarishche <35015540+makarishche@users.noreply.github.com>
2018-01-06 13:39:37 +0200

    Update script.py
    Пробуем еще

--- script.py
+++ script.py
@@ -1,38 +1,31 @@
-import requests
+import requests  
+import datetime
 
-url = "https://api.telegram.org/bot478682565:AAG2dPlwCeg-mV9qYSOEEuRgbB1rTPK_jNQ/"
+class BotHandler:
 
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
-def get_updates_json(request):  
-    response = requests.get(request + 'getUpdates')
-    return response.json()
+    def get_updates(self, offset=None, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
 
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
 
-def last_update(data):  
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
-    
-    
-def get_chat_id(update):  
-    chat_id = update['message']['chat']['id']
-    return chat_id
+    def get_last_update(self):
+        get_result = self.get_updates()
 
-def send_mess(chat, text):  
-    params = {'chat_id': chat, 'hui-pizda-malafya': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
 
-chat_id = get_chat_id(last_update(get_updates_json(url)))
-send_mess(chat_id, '')
-
-def main():  
-    update_id = last_update(get_updates_json(url))['update_id']
-    while True:
-        if update_id == last_update(get_updates_json(url))['update_id']:
-           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
-           update_id += 1
-        sleep(1)       
-
-if __name__ == '__main__':  
-    main()
+        return last_update
